faaad6d2c96a73e31320b8ae34f2b45d7cab44e4
kevinrajgor <kevinrajg95@gmail.com>
2023-01-02 18:47:32 +0530

    Problem Solved

--- Solutin 2.java
+++ Solutin 2.java
@@ -1,41 +1,32 @@
 /*
- * You are playing the Bulls and Cows game with your friend.
+ * Find all valid combinations of k numbers that sum up to n such that the
+ * following conditions are true:
  * 
- * You write down a secret number and ask your friend to guess what the number
- * is. When your friend makes a guess, you provide a hint with the following
- * info:
- * 
- * The number of "bulls", which are digits in the guess that are in the correct
- * position.
- * The number of "cows", which are digits in the guess that are in your secret
- * number but are located in the wrong position. Specifically, the non-bull
- * digits in the guess that could be rearranged such that they become bulls.
- * Given the secret number secret and your friend's guess guess, return the hint
- * for your friend's guess.
- * 
- * The hint should be formatted as "xAyB", where x is the number of bulls and y
- * is the number of cows. Note that both secret and guess may contain duplicate
- * digits.
- * 
+ * Only numbers 1 through 9 are used.
+ * Each number is used at most once.
+ * Return a list of all possible valid combinations. The list must not contain
+ * the same combination twice, and the combinations may be returned in any
+ * order.
  */
 
-public class Solution {
-    public String getHint(String secret, String guess) {
-        int len = secret.length();
-        int[] secretArr = new int[10];
-        int[] guessArr = new int[10];
-        int bull = 0, cow = 0;
-        for (int i = 0; i < len; ++i) {
-            if (secret.charAt(i) == guess.charAt(i)) {
-                ++bull;
-            } else {
-                ++secretArr[secret.charAt(i) - '0'];
-                ++guessArr[guess.charAt(i) - '0'];
-            }
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        combi(result, new ArrayList<>(), 1, 0, k, n);
+        return result;
+    }
+
+    public void combi(List<List<Integer>> result, List<Integer> arr, int num, int sum, int k, int n) {
+        if (k == 0 && n == sum) {
+            result.add(new ArrayList<>(arr));
+            return;
         }
-        for (int i = 0; i < 10; ++i) {
-            cow += Math.min(secretArr[i], guessArr[i]);
+        for (int i = num; i <= 9; i++) {
+            sum += i;
+            arr.add(i);
+            combi(result, arr, i + 1, sum, k - 1, n);
+            sum -= i;
+            arr.remove(arr.size() - 1);
         }
-        return "" + bull + "A" + cow + "B";
     }
 }
